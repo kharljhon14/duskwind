61989da9de9cfb6d9884fdbc95653450ae9f8932
kharle14 <enriquezkharl14@gmail.com>
2024-04-22 22:26:47 +0800

    [Feat] Added Sponsors feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/header/Header';
 import Hero from './features/hero/Hero';
+import Sponsors from './features/sponsors/Sponsors';
 
 export default function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <Sponsors />
     </div>
   );
 }
---
+++ src/features/sponsors/Sponsors.tsx
@@ -0,0 +1,28 @@
+import './sponsor.css';
+
+export default function Sponsors() {
+  return (
+    <div className="sponsors">
+      <img
+        src="/sponsors/amazon_logo.svg"
+        alt="Amazon logo"
+      />
+      <img
+        src="/sponsors/behance_logo.svg"
+        alt="Behance logo"
+      />
+      <img
+        src="/sponsors/faceBook_logo.svg"
+        alt="Facebook logo"
+      />
+      <img
+        src="/sponsors/github_logo.svg"
+        alt="Github logo"
+      />
+      <img
+        src="/sponsors/vercel_logo.svg"
+        alt="Vercel logo"
+      />
+    </div>
+  );
+}
